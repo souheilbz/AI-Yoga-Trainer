20c5351d801e51b54157b928992b33d35cabb16a
Souheil Bouzouita <souheilb15@gmail.com>
2022-03-28 12:40:16 +0100

    Update About.js

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -12,10 +12,10 @@ export default function About() {
                     I created this as a personal project, and I have also deployed this project
                     so people can use it and mainly the developers can who are learning AI can learn 
                     from this project and make their own AI or they can also improve in this project.
-                    This is an open source project, The code is available on the GitHub - <a href="https://github.com/souheilbz/AI-Yoga-Trainer">https://github.com/harshbhatt7585/YogaIntelliJ</a>
+                    This is an open source project, The code is available on the GitHub - <a href="https://github.com/souheilbz/AI-Yoga-Trainer">https://github.com/souheilbz</a>
                     
                     This AI first predicts keypoints or coordinates of different parts of the body(basically where
-                    they are present in an image) and then it use another classification model to classify the poses if 
+                    they are present in an image) and then it uses another classification model to classify the poses if 
                     someone is doing a pose and if AI detects that pose more than 95% probability and then it will notify you are 
                     doing correctly(by making virtual skeleton green). I have used Tensorflow pretrained Movenet Model To Predict the 
                     Keypoints and building a neural network top of that which uses these coordinates and classify a yoga pose.
